349c647b374aaad389b6e1495a93bf1a78cb45fd
Dorian Vallecillo Calderon <dorianv@microsoft.com>
2022-03-05 20:34:00 -0600

    switch case

--- main.go
+++ main.go
@@ -2,37 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strconv"
 )
 
 func main() {
-	// Condicional If
-	valor1:=1
-	valor2:=2
+	//switch case
 
-	if valor1==1{
-		fmt.Println("Es 1")
-	}else{
-		fmt.Println("No es 1")
+	
+	switch modulo := 5 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
 	}
 
-	//With and
-
-	if valor1 == 1 && valor2==2{
-		fmt.Println("Es verdad")
+	//Sin condicion
+	value:=200
+	switch{
+	case value>100:
+		fmt.Println("Es mayor a 100")
+	case value<0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
 	}
-
-	//with or
-	if valor1==0 || valor2==2{
-		fmt.Println("Es verdad, OR")
-	}
-
-	//Convertir texto a numero
-	value, err:= strconv.Atoi("52")
-	if err!= nil{
-		log.Fatal(err)
-	}
-	fmt.Println("Value: ", value)
-
 }
